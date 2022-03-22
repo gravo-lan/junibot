45911d897214f6107cf6ca17900622d72c15cc0f
GravoLan <66149576+gravo-lan@users.noreply.github.com>
2022-03-22 14:34:10 +0800

    Update tools.py

--- gameplay/tools.py
+++ gameplay/tools.py
@@ -1,22 +1,22 @@
 import json
 
 def add_tool_member(person):
-    data = json.load(open("tools.txt"))
+    data = json.load(open("./tools.json"))
     if person not in data:
         data[person] = {}
         data[person]["pickaxe"] = "basic"
         data[person]["axe"] = "basic"
         data[person]["hoe"] = "basic"
         data[person]["watering can"] = "basic"
-        json.dump(data, open("tools.txt", "w"))
+        json.dump(data, open("./tools.json", "w"))
 
 def change_tool(value, tool, person):
-    data = json.load(open("tools.txt"))
+    data = json.load(open("./tools.json"))
     value = str(value.lower())
     data[person][tool] = value
-    json.dump(data, open("tools.txt", "w"))
+    json.dump(data, open("./tools.json", "w"))
 
 def find_tool(tool, person):
-    data = json.load(open("tools.txt"))
+    data = json.load(open("./tools.json"))
     thetool = data[person][tool]
     return thetool
