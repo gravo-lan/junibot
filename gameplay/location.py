38f9a68ad3ead19d18a6c91c964e1ee556fd0e79
GravoLan <66149576+gravo-lan@users.noreply.github.com>
2022-03-22 14:33:39 +0800

    Update location.py

--- gameplay/location.py
+++ gameplay/location.py
@@ -1,19 +1,19 @@
 import json
 
 def add_location_member(person):
-    data = json.load(open("location.txt"))
+    data = json.load(open("./location.json"))
     if person not in data:
         data[person] = "farm"
-        json.dump(data, open("location.txt", "w"))
+        json.dump(data, open("./location.json", "w"))
 
 def change_location(location, person):
-    data = json.load(open("location.txt"))
+    data = json.load(open("./location.json"))
     location = str(location.lower())
     data[person] = location
-    json.dump(data, open("location.txt", "w"))
+    json.dump(data, open("./location.json", "w"))
 
 
 def find_location(person):
-    data = json.load(open("location.txt"))
+    data = json.load(open("./location.json"))
     location = data[person]
     return location
