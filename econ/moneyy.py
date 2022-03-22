14e13ec73912bcc7043e7ac58d46b36d74803845
GravoLan <66149576+gravo-lan@users.noreply.github.com>
2022-03-22 14:12:42 +0800

    Fixed filepath to money.json

--- econ/moneyy.py
+++ econ/moneyy.py
@@ -1,22 +1,22 @@
 import json
 
 def add_money(added_number, author):
-    coins = json.load(open('money.txt', 'r'))
+    coins = json.load(open('./money.json', 'r'))
     matchedLine = int(coins[str(author)])
     newvalue = matchedLine + added_number
     coins[str(author)] = str(newvalue)
-    json.dump(coins, open('money.txt', 'w'))
+    json.dump(coins, open('./money.json', 'w'))
 
 def remove_money(removed_number, author):
-    coins = json.load(open('money.txt', 'r'))
+    coins = json.load(open('./money.json', 'r'))
     matchedLine = int(coins[str(author)])
     newvalue = matchedLine - removed_number
     if newvalue <= 0:
         newvalue = 0
     coins[str(author)] = str(newvalue)
-    json.dump(coins, open('money.txt', 'w'))
+    json.dump(coins, open('./money.json', 'w'))
 
 def find_money(author):
-    file = json.load(open('money.txt', 'r'))
+    file = json.load(open('./money.json', 'r'))
     matchedLine = file[str(author)]
     return int(matchedLine)
